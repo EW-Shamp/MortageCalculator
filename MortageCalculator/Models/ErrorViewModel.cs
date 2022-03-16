547e29c87efc3466052720d230c38253898ddc71
EW-Shamp <ew_shamp@post.com>
2022-03-16 06:22:44 -0400

    Initial commit

---
+++ MortageCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MortageCalculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MortageCalculator
+ working app
